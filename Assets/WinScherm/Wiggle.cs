7b01f6e5f8f7bf0d1e946330045279729e8d01e6
Thomas de Boer <twjdeboer@users.noreply.github.com>
2023-07-27 22:16:26 +0200

    Add Wiggle Effect

--- Assets/WinScherm/Wiggle.cs
+++ Assets/WinScherm/Wiggle.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 
 public class Wiggle : MonoBehaviour
 {
+    private float _TShake = 11.0f;
+    private float _degShake = 5;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +17,6 @@ public class Wiggle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.rotation = quaternion.EulerXYZ(0, 0, UnityEngine.Random.Range(-0.1f, 0.1f));
+        gameObject.transform.rotation = quaternion.EulerXYZ(0, 0, _degShake/360*math.cos(Time.time/_TShake*2*math.PI));
     }
 }
