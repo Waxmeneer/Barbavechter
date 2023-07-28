9ac7eda03cabf698138536870f012c965b0e9175
Thomas de Boer <twjdeboer@users.noreply.github.com>
2023-07-28 02:06:53 +0200

    Improve Crying Animation

--- Assets/WinScherm/Crying.cs
+++ Assets/WinScherm/Crying.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 
 public class Crying : MonoBehaviour
 {
-    private float _TShake = 5.0f;
-    private float _degShake = 0.5f;
+    public float TShake = 5.0f;
+    public float degShake = 0.4f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +15,6 @@ public class Crying : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.rotation *= quaternion.EulerXYZ(0, 0, _degShake / 360 * math.cos(Time.time / _TShake * 2 * math.PI));
+        gameObject.transform.rotation *= quaternion.EulerXYZ(0, 0, degShake / 360.0f * math.cos(Time.time / TShake * 2 * math.PI));
     }
 }
